defae8e87c55a200a51b544ab52c873b672e3dc3
Frank Buono <fbuonojr@fordham.edu>
2020-12-09 21:28:40 -0500

    finished ? helper function

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,12 @@
 var connection = require("../config/connection.js");
 
 //this is a helper function to print question marks for sql syntax
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++){
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
