f1613307260b800d82d07712b28b0858f317149e
Frank Buono <fbuonojr@fordham.edu>
2020-12-09 21:15:37 -0500

    connection variable for mysql

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,10 @@
 //MySQL connection
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "cat_db"
+});
