d3fbcb24383b5a3f5197cfb3ca75c7f755262ac1
Frank Buono <fbuonojr@fordham.edu>
2020-12-09 21:39:09 -0500

    helper function

--- config/orm.js
+++ config/orm.js
@@ -10,3 +10,22 @@ function printQuestionMarks(num) {
 
     return arr.toString();
 }
+
+//helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    for(var key in ob) {
+        var value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)){
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
