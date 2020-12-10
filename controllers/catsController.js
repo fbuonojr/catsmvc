92cab71adc627f5dc8077d0fcc2d77d62dfa6b5d
Frank Buono <fbuonojr@fordham.edu>
2020-12-10 16:40:12 -0500

    finished cat controller

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -4,3 +4,41 @@ var router = express.Router();
 
 //model for database functions
 var cat = require("../models/cat.js");
+
+//routes
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+        var hbsObject = {
+            cats: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/cats", function(req, res) {
+    cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    cat.update(
+        {
+            sleepy: req.body.sleepy
+        },
+        condition,
+        function(result) {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
