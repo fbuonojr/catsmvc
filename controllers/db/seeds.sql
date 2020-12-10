0070b06aa4aa497e54542ab894e2563d5b88cbfa
Frank Buono <fbuonojr@fordham.edu>
2020-12-10 16:59:55 -0500

    seeds

---
+++ controllers/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobby');
+INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO cats (name, sleepy) VALUES ('Marilyn', true);
+INSERT INTO cats (name, sleepy) VALUES ('Joe', true);
+INSERT INTO cats (name) VALUES ('Owens');
