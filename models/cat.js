3c8e1842b2148fd51dea98e3a35a6dfaebdddd36
Frank Buono <fbuonojr@fordham.edu>
2020-12-15 11:27:31 -0500

    finished cat model

--- models/cat.js
+++ models/cat.js
@@ -5,5 +5,17 @@ var cat = {
         orm.all("cats", function(res) {
             cb(res);
         });
+    },
+    create: function(cols, vals, cb){
+        orm.create("cats", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("cats", objColVals, condition, function(res) {
+            cb(res);
+        });
     }
-}
+};
+
+module.exports = cat;
