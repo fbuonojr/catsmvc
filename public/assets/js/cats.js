3c57d7dd99e9142b20559a7085ddae79817ae752
Frank Buono <fbuonojr@fordham.edu>
2020-12-15 13:49:05 -0500

    fixed bug

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".change-sleep").on("click", function(event) {
         var id = $(this).data("id");
-        var newSleep = $(this).data("newSleep");
+        var newSleep = $(this).data("newsleep");
 
         var newSleepState = {
             sleepy: newSleep
