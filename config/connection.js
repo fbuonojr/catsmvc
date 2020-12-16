75c4bc562e3eb8ca835c66b8b9fde8652ba56b52
Frank Buono <fbuonojr@fordham.edu>
2020-12-16 10:35:22 -0500

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 //MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "cat_db"
-});
+var connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "cat_db"
+    });
+}
 
 //connect
-connection.connect(function(err) {
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         console.error("error connectiong: " + err.stack);
         return;
     }
